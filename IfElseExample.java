6177a6f057246b3c941a14a665e053550151fdba
Sanika Phadtare <sanikaphadtare70@gmail.com>
2025-09-19 11:42:20 +0530

    Add files via upload

---
+++ IfElseExample.java
@@ -0,0 +1,11 @@
+public class IfElseExample {
+    public static void main(String[] args) {
+        int number = -5;
+
+        if (number >= 0) {
+            System.out.println(number + " is positive.");
+        } else {
+            System.out.println(number + " is negative.");
+        }
+    }
+}
