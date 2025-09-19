bac3bcfeb559bd2000dd92106303034eaea25a55
Sanika Phadtare <sanikaphadtare70@gmail.com>
2025-09-19 11:49:36 +0530

    Add files via upload

---
+++ ArithmeticOperators.java
@@ -0,0 +1,12 @@
+public class ArithmeticOperators {
+    public static void main(String[] args) {
+        int a = 20, b = 10;
+
+        System.out.println("Arithmetic Operators:");
+        System.out.println("a + b = " + (a + b));
+        System.out.println("a - b = " + (a - b));
+        System.out.println("a * b = " + (a * b));
+        System.out.println("a / b = " + (a / b));
+        System.out.println("a % b = " + (a % b));
+    }
+}
